b78affcd73f479488e845a0118ad54d717feb016
moinkhao1024 <moinkhao@proton.me>
2025-05-31 18:40:58 +0800

    Update index.ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from "vue-router";
-import type { RouteRecordRaw } from "vue-router";
+import type { RouteRecordRaw, RouteLocationNormalized, NavigationGuardNext, RouterScrollBehavior } from "vue-router";
 import HomeView from "@/views/HomeView.vue";
 
 const routes: RouteRecordRaw[] = [
@@ -86,19 +86,21 @@ const routes: RouteRecordRaw[] = [
   },
 ];
 
+const scrollBehavior: RouterScrollBehavior = (to, from, savedPosition) => {
+  if (savedPosition) {
+    return savedPosition;
+  }
+  return { top: 0 };
+};
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
-  scrollBehavior(to, from, savedPosition) {
-    if (savedPosition) {
-      return savedPosition;
-    }
-    return { top: 0 };
-  },
+  scrollBehavior,
 });
 
 // 路由标题
-router.beforeEach((to, from, next) => {
+router.beforeEach((to: RouteLocationNormalized, from: RouteLocationNormalized, next: NavigationGuardNext) => {
   document.title = `${to.meta.title || "首页"} | Handsome`;
   next();
 });
